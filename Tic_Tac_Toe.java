b9438d4a479edeab5c376248e9c9e28baeb97d4c
rajhans.a <rajhans.a@internal.media.net>
2023-08-01 18:12:32 +0530

    adding the main tic-tac-toe file

---
+++ Tic_Tac_Toe.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Tic_Tac_Toe {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Welcome to Tic-Tac-Toe");
+        System.out.println("Enter 1 : To play a Simple Tic-Tac-Toe Game");
+        System.out.println("Enter 2 : To play a Tournament");
+        System.out.println("Enter 3 : To play Against A Bot ");
+        int n=obj.nextInt();
+        if(n==1)
+        {
+
+        }
+        else if(n==2)
+        {
+
+        }
+        else if(n==3)
+        {
+
+        }
+        else
+        {
+            System.out.println("INVALID OPTION");
+        }
+    }
+}
